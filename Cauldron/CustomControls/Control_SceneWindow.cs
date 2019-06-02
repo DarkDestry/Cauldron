4c5fa514e9c1870a2787387935eea46768616f12
DarkDestry <tanki123@hotmail.com>
2019-06-03 01:22:49 +0800

    Added Turntable Orbit camera and origin grid lines.
    Removed old WPF canvas view

--- Cauldron/CustomControls/Control_SceneWindow.cs
+++ Cauldron/CustomControls/Control_SceneWindow.cs
@@ -1,21 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Cauldron.Primitives;
 
 namespace Cauldron.CustomControls
 {
@@ -39,7 +24,7 @@ namespace Cauldron.CustomControls
         }
 
 
-        private Canvas front, right, top;
+        //private Canvas front, right, top;
         private MonoGameViewport perspective;
         private int frontZoom = 8;
         private int rightZoom = 8;
@@ -54,115 +39,17 @@ namespace Cauldron.CustomControls
                  
             if (Template != null)
             {
-                front = Template.FindName("Canvas_Front", this) as Canvas;
-                right = Template.FindName("Canvas_Right",this) as Canvas;
-                top = Template.FindName("Canvas_Top",this) as Canvas;
-                perspective = Template.FindName("Viewport_3D",this) as MonoGameViewport;
-
-                front.GotMouseCapture += HandleMouseMove;
-                right.GotMouseCapture += HandleMouseMove;
-                top.GotMouseCapture += HandleMouseMove;
-
-                front.MouseDown += Canvas_MouseDown;
-                right.MouseDown += Canvas_MouseDown;
-                top.MouseDown += Canvas_MouseDown;
-
-                front.MouseMove += HandleMouseMove;
-                right.MouseMove += HandleMouseMove;
-                top.MouseMove += HandleMouseMove;
-
-                front.MouseWheel += (sender, args) =>
-                {
-                    if (Math.Sign(args.Delta) > 0) frontZoom *= 2;
-                    else frontZoom /= 2;
-                    UpdateCanvas();
-                };
-                right.MouseWheel += (sender, args) =>
-                {
-                    if (Math.Sign(args.Delta) > 0) rightZoom *= 2;
-                    else rightZoom /= 2;
-                    UpdateCanvas();
-                };
-                top.MouseWheel += (sender, args) =>
-                {
-                    if (Math.Sign(args.Delta) > 0) topZoom *= 2;
-                    else topZoom /= 2;
-                    UpdateCanvas();
-                };
-
-                Hierarchy.HierarchyUpdateEvent += UpdateCanvas;
-                SceneObjectFocusEvent += FocusObject;
             }
-
-            UpdateCanvas();
         }
 
         private void FocusObject(Hierarchy.SceneObject obj)
         {
             focusPoint = obj.Transform.Position;
-            UpdateCanvas();
         }
 
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Mouse.Capture(sender as IInputElement);
-        }
-
-        private void UpdateCanvas()
-        {
-            front.Children.Clear();
-            right.Children.Clear();
-            top.Children.Clear();
-            //perspective.Children.Clear();
-            Label test = new Label();
-            test.Content = $"{focusPoint.x}, {focusPoint.y}, {focusPoint.z}";
-            front.Children.Add(test);
-
-            //canvas top left points
-            float frontX = focusPoint.x - (float) front.ActualWidth / 2 / frontZoom;
-            float frontY = focusPoint.y + (float) front.ActualHeight / 2 / frontZoom;
-            float rightX = focusPoint.z + (float) right.ActualWidth / 2 / rightZoom;
-            float rightY = focusPoint.y + (float) right.ActualHeight / 2 / rightZoom; 
-            float topX = focusPoint.x - (float) top.ActualWidth / 2 / topZoom;
-            float topY = focusPoint.z - (float)top.ActualHeight / 2 / topZoom;
-
-            foreach (var sceneObject in Hierarchy.hierarchyObjectList)
-            {
-                //Top Canvas
-                Ellipse topSphere = new Ellipse();
-                topSphere.Width = Math.Abs(sceneObject.Transform.Scale.x * topZoom);
-                topSphere.Height = Math.Abs(sceneObject.Transform.Scale.z * topZoom);
-                topSphere.Stroke = Brushes.White;
-                topSphere.StrokeThickness = 1;
-                Canvas.SetLeft(topSphere, (sceneObject.Transform.Position.x - topX - sceneObject.Transform.Scale.x / 2) * topZoom);
-                Canvas.SetTop(topSphere, (sceneObject.Transform.Position.z - topY - sceneObject.Transform.Scale.z / 2) * topZoom);
-                top.Children.Add(topSphere);
-
-                //Right Canvas
-                Ellipse rightSphere = new Ellipse();
-                rightSphere.Width = Math.Abs(sceneObject.Transform.Scale.z * rightZoom);
-                rightSphere.Height = Math.Abs(sceneObject.Transform.Scale.y * rightZoom);
-                rightSphere.Stroke = Brushes.White;
-                rightSphere.StrokeThickness = 1;
-                Canvas.SetLeft(rightSphere,
-                    (rightX - sceneObject.Transform.Position.z - sceneObject.Transform.Scale.z / 2) * rightZoom);
-                Canvas.SetTop(rightSphere,
-                    (rightY - sceneObject.Transform.Position.y - sceneObject.Transform.Scale.y / 2) * rightZoom);
-                right.Children.Add(rightSphere);
-
-                //Front Canvas
-                Ellipse frontSphere = new Ellipse();
-                frontSphere.Width = Math.Abs(sceneObject.Transform.Scale.x * frontZoom);
-                frontSphere.Height = Math.Abs(sceneObject.Transform.Scale.y * frontZoom);
-                frontSphere.Stroke = Brushes.White;
-                frontSphere.StrokeThickness = 1;
-                Canvas.SetLeft(frontSphere,
-                    (sceneObject.Transform.Position.x - frontX - sceneObject.Transform.Scale.x / 2) * frontZoom);
-                Canvas.SetTop(frontSphere,
-                    (frontY - sceneObject.Transform.Position.y - sceneObject.Transform.Scale.y / 2) * frontZoom);
-                front.Children.Add(frontSphere);
-            }
-
         }
 
         private bool panning;
@@ -207,8 +94,6 @@ namespace Cauldron.CustomControls
                     Mouse.Capture(null);
                 }
             }
-            UpdateCanvas();
-
         }
     }
 }
--- Cauldron/MonoGameViewport.cs
+++ Cauldron/MonoGameViewport.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.Framework.WpfInterop;
 using MonoGame.Framework.WpfInterop.Input;
-using SharpDX.Direct3D9;
+using SharpDX.Direct2D1;
 using Box = Cauldron.Primitives.Box;
 using Point = Microsoft.Xna.Framework.Point;
 using PrimitiveType = Microsoft.Xna.Framework.Graphics.PrimitiveType;
+using Quaternion = Microsoft.Xna.Framework.Quaternion;
 
 namespace Cauldron
 {
@@ -36,6 +33,8 @@ namespace Cauldron
             _mouse = new WpfMouse(this);
 
             effect = new BasicEffect(GraphicsDevice);
+            effect.EnableDefaultLighting();
+            effect.SpecularColor = Vector3.Zero;
 
             // must be called after the WpfGraphicsDeviceService instance was created
             base.Initialize();
@@ -50,24 +49,31 @@ namespace Cauldron
             var keyboardState = _keyboard.GetState();
         }
 
-        private float cameraPitch = 0, cameraYaw = 0;
-        private Vector3 cameraPos = new CldVector3(0, 0, 30);
-        private Vector3 cameraVector => Vector3.Transform(Vector3.Forward, Matrix.CreateFromYawPitchRoll(MathHelper.ToRadians(cameraYaw), MathHelper.ToRadians(cameraPitch), 0));
-        private Vector3 cameraRight => Vector3.Normalize(Vector3.Cross(cameraVector, Vector3.Up));
+        private float cameraPitch;
+        private float cameraYaw;
+        private float distance = 30;
+        private Vector3 cameraFocusPoint = new CldVector3(0,0,0);
+
+        private Matrix cameraTransform => Matrix.CreateFromYawPitchRoll(MathHelper.ToRadians(cameraYaw), MathHelper.ToRadians(cameraPitch), 0);
+
+        private Vector3 cameraForward => Vector3.Transform(Vector3.Forward, cameraTransform);
+        private Vector3 cameraRight => Vector3.Transform(Vector3.Right, cameraTransform);
+        private Vector3 cameraUp => Vector3.Transform(Vector3.Up, cameraTransform);
+
+        private Vector3 cameraPosition => cameraFocusPoint - cameraForward * distance;
 
         private float lastTotalTime = 0;
 
         protected override void Draw(GameTime time)
         {
             float deltaTime = (float)time.ElapsedGameTime.Milliseconds/1000;
-            System.Diagnostics.Trace.WriteLine(deltaTime);
 
             HandleInput(deltaTime);
 
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(new Color(new Vector3(0.14f)));
 
 
-            effect.View = Matrix.CreateLookAt(cameraPos, cameraPos+cameraVector, new CldVector3(0,1,0));
+            effect.View = Matrix.CreateLookAt(cameraPosition, cameraFocusPoint, cameraUp);
 
             float aspectRatio = (float) (ActualWidth / ActualHeight);
             float fieldOfView = MathHelper.PiOver4;
@@ -77,8 +83,42 @@ namespace Cauldron
             effect.Projection = Matrix.CreatePerspectiveFieldOfView(
                 fieldOfView, aspectRatio, nearClipPlane, farClipPlane);
 
-            effect.EnableDefaultLighting();
-            
+            //effect.VertexColorEnabled = true;
+
+            int gridLineCount = 8;
+
+            VertexPositionNormalTexture[] worldGrid = new VertexPositionNormalTexture[(gridLineCount*2 +1)* 4];
+
+            for (int i = -gridLineCount; i <= gridLineCount; i++)
+            {
+                if (i == 0) continue;
+                worldGrid[(i + gridLineCount) * 4 + 0].Position = new Vector3(-gridLineCount, 0, i);
+                worldGrid[(i + gridLineCount) * 4 + 1].Position = new Vector3(gridLineCount, 0, i);
+                worldGrid[(i + gridLineCount) * 4 + 2].Position = new Vector3(i, 0, gridLineCount);
+                worldGrid[(i + gridLineCount) * 4 + 3].Position = new Vector3(i, 0, -gridLineCount);
+            }
+
+            for (var i = 0; i < worldGrid.Length; i++)
+            {
+                worldGrid[i].Normal = Vector3.Up;
+                worldGrid[i].TextureCoordinate = Vector2.Zero; 
+            }
+
+            var forwardLine = new[]
+            {
+                new VertexPositionNormalTexture(Vector3.Forward * 10, Vector3.Up, Vector2.Zero),
+                new VertexPositionNormalTexture(Vector3.Forward * -10, Vector3.Up, Vector2.Zero)
+            };
+            var rightLine = new[]
+            {
+                new VertexPositionNormalTexture(Vector3.Right * -10, Vector3.Up, Vector2.Zero),
+                new VertexPositionNormalTexture(Vector3.Right * 10, Vector3.Up, Vector2.Zero)
+            };
+            var upLine = new[]
+            {
+                new VertexPositionNormalTexture(Vector3.Up * -10, Vector3.Up, Vector2.Zero),
+                new VertexPositionNormalTexture(Vector3.Up * 10, Vector3.Up, Vector2.Zero)
+            };
 
             GraphicsDevice.DepthStencilState = DepthStencilState.Default;
 
@@ -87,6 +127,8 @@ namespace Cauldron
                 pass.Apply();
                 foreach (var sceneObject in Hierarchy.hierarchyObjectList)
                 {
+                    effect.DiffuseColor = new Vector3(1,1,1);;
+                    pass.Apply();
                     Box box = new Box(sceneObject.Transform);
 
                     GraphicsDevice.DrawUserPrimitives(
@@ -95,39 +137,84 @@ namespace Cauldron
                         0,
                         12);
 
-
                 }
+
+                effect.DiffuseColor = new Vector3(0.3f); ;
+                pass.Apply();
+                GraphicsDevice.DrawUserPrimitives(
+                    PrimitiveType.LineList,
+                    worldGrid,
+                    0,
+                    worldGrid.Length/2
+                );
+
+                effect.DiffuseColor = new Vector3(0.1f, 0.1f, 0.6f); ;
+                pass.Apply();
+                GraphicsDevice.DrawUserPrimitives(
+                    PrimitiveType.LineList,
+                    forwardLine,
+                    0,
+                    1
+                );
+
+                effect.DiffuseColor = new Vector3(0.6f, 0.1f, 0.1f); ;
+                pass.Apply();
+                GraphicsDevice.DrawUserPrimitives(
+                    PrimitiveType.LineList,
+                    rightLine,
+                    0,
+                    1
+                );
+
+                //effect.DiffuseColor = new Vector3(0.1f, 0.6f, 0.1f); ;
+                //pass.Apply();
+                //GraphicsDevice.DrawUserPrimitives(
+                //    PrimitiveType.LineList,
+                //    upLine,
+                //    0,
+                //    1
+                //);
             }
 
         }
 
         private Point pos;
+        private int lastScrollWheelValue;
+        private bool orbiting;
 
         private void HandleInput(float deltaTime)
         {
-            if (_mouse.GetState().RightButton == ButtonState.Pressed)
+            if (_mouse.GetState().MiddleButton == ButtonState.Pressed)
             {
                 if (!_mouse.CaptureMouseWithin) pos = _mouse.GetState().Position;
 
                 _mouse.CaptureMouseWithin = true;
+                orbiting = true;
             }
-            else _mouse.CaptureMouseWithin = false;
+            else
+            {
+                _mouse.CaptureMouseWithin = false;
+                orbiting = false;
+            }
 
-            if (_mouse.CaptureMouseWithin)
+            if (orbiting)
             {
                 Point delta = _mouse.GetState().Position - pos;
 
                 pos = _mouse.GetState().Position;
 
-                cameraPitch += (float)-delta.Y/2;
-                cameraYaw += (float)-delta.X/2;
+                cameraPitch += (float) -delta.Y / 2;
+                cameraYaw += (float)-delta.X / 2;
+            }
 
-                if (_keyboard.GetState().IsKeyDown(Keys.W)) cameraPos += cameraVector * deltaTime * 2;
-                if (_keyboard.GetState().IsKeyDown(Keys.S)) cameraPos -= cameraVector * deltaTime * 2;
-                if (_keyboard.GetState().IsKeyDown(Keys.D)) cameraPos += cameraRight * deltaTime * 2;
-                if (_keyboard.GetState().IsKeyDown(Keys.A)) cameraPos -= cameraRight * deltaTime * 2;
-                if (_keyboard.GetState().IsKeyDown(Keys.Q)) cameraPos += Vector3.Up * deltaTime * 2;
-                if (_keyboard.GetState().IsKeyDown(Keys.E)) cameraPos -= Vector3.Up * deltaTime * 2;
+            if (lastScrollWheelValue != _mouse.GetState().ScrollWheelValue)
+            {
+                int delta = _mouse.GetState().ScrollWheelValue - lastScrollWheelValue;
+                lastScrollWheelValue = _mouse.GetState().ScrollWheelValue;
+
+                distance -= delta / 10;
+                distance = MathHelper.Clamp(distance, 1, float.MaxValue);
+                System.Diagnostics.Trace.WriteLine(distance.ToString());
             }
         }
     }
