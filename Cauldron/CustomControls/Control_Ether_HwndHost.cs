b2347b9a5daef319948b3df9b2b8212cd4cd3856
DarkDestry <tanki123@hotmail.com>
2020-08-08 18:02:26 +0800

    Add support for Ether
    Add Mouse Input support for Ether
    Add Keyboard Input support for Ether
    Add Resize support for Ether
    
    Remove child hwnd creation as Ether will be creating the window

--- Cauldron/CustomControls/Control_Ether_HwndHost.cs
+++ Cauldron/CustomControls/Control_Ether_HwndHost.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Cauldron.CustomControls
 {
@@ -27,6 +20,7 @@ namespace Cauldron.CustomControls
 
         private RendererHost rendererControl;
         private Border hwndHostElement;
+        private DispatcherTimer timer = new DispatcherTimer();
 
         public override void OnApplyTemplate()
         {
@@ -36,37 +30,74 @@ namespace Cauldron.CustomControls
             {
                 hwndHostElement = Template.FindName("HwndHostElement", this) as Border;
 
-                CompositionTarget.Rendering += OnCompositionTargetOnRendering;
+                this.Loaded += (sender, args) => InitEther();
+                this.SizeChanged += OnSizeChanged;
             }
         }
 
-        private bool hostContainerInitialized = false;
-        private void OnCompositionTargetOnRendering(object sender, EventArgs args)
+        private bool etherInitialized = false;
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
-            if (!hostContainerInitialized)
-            {
-                rendererControl = new RendererHost(hwndHostElement.ActualHeight, hwndHostElement.ActualWidth);
-                hwndHostElement.Child = rendererControl;
-                Ether.Initialize(rendererControl.HwndRenderer);
-                hostContainerInitialized = true;
-            }
-            Ether.Update();
+            if (!etherInitialized) return;
+            Size size = GetElementPixelSize(this);
+            Ether.ResizeViewport((uint)size.Width, (uint)size.Height);
         }
 
+        private void InitEther()
+        {
+            Size size = GetElementPixelSize(this);
+            rendererControl = new RendererHost(size.Height, size.Width);
+            hwndHostElement.Child = rendererControl;
+            rendererControl.MessageHook += new HwndSourceHook(WndProcMsgFilter);
+            Ether.ResizeViewport((uint)size.Width, (uint)size.Height);
+            timer.Tick += (o, eventArgs) => {
+                Ether.Update();
+                Ether.Render();
+            };
+            timer.Start();
+            etherInitialized = true;
+        }
+
+        public Size GetElementPixelSize(UIElement element)
+        {
+            Matrix transformToDevice;
+            var source = PresentationSource.FromVisual(element);
+            if (source != null)
+                transformToDevice = source.CompositionTarget.TransformToDevice;
+            else
+                using (var source2 = new HwndSource(new HwndSourceParameters()))
+                    transformToDevice = source2.CompositionTarget.TransformToDevice;
+
+            Size transformedSize =(Size)transformToDevice.Transform((Vector)element.RenderSize);
+            transformedSize.Height = Math.Ceiling(transformedSize.Height);
+            transformedSize.Width = Math.Ceiling(transformedSize.Width);
+            return transformedSize;
+        }
+
+        private IntPtr WndProcMsgFilter(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg.Equals(0x0021)) RendererHost.SendMessage(rendererControl.HwndRenderer, 0x0006, new IntPtr(1), new IntPtr(0));
+            handled = false;
+            return IntPtr.Zero;
+        }
 
         class Ether
         {
             [DllImport("Ether.dll")]
-            public static extern void Initialize(IntPtr window);
+            public static extern IntPtr Initialize(IntPtr hwndParent);
 
             [DllImport("Ether.dll")]
             public static extern void Update();
 
             [DllImport("Ether.dll")]
+            public static extern void Render();
+
+            [DllImport("Ether.dll")]
             public static extern void Release();
 
             [DllImport("Ether.dll")]
-            public static extern IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam);
+            public static extern void ResizeViewport(uint width, uint height);
         }
 
         public class RendererHost : HwndHost
@@ -110,27 +141,15 @@ namespace Cauldron.CustomControls
                     IntPtr.Zero,
                     0);
 
-                hwndRenderer = CreateWindowEx(0, "static", "",
-                    WS_CHILD | WS_VISIBLE,
-                    0, 0,
-                    hostWidth, hostHeight,
-                    hwndHost,
-                    (IntPtr)CHILD_ID,
-                    IntPtr.Zero,
-                    0);
+                hwndRenderer = Ether.Initialize(hwndHost);
 
                 return new HandleRef(this, hwndHost);
             }
 
             protected override void DestroyWindowCore(HandleRef hwnd)
             {
+                Ether.Release();
                 DestroyWindow(hwnd.Handle);
-            }
-
-            protected override IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-            {
-                handled = false;
-                return IntPtr.Zero;
             }
 
             [DllImport("user32.dll", EntryPoint = "DestroyWindow", CharSet = CharSet.Unicode)]
@@ -148,6 +167,24 @@ namespace Cauldron.CustomControls
                 IntPtr hMenu,
                 IntPtr hInst,
                 [MarshalAs(UnmanagedType.AsAny)] object pvParam);
+
+            [DllImport("user32.dll", EntryPoint = "SendMessage", CharSet = CharSet.Unicode)]
+            internal static extern int SendMessage(IntPtr hwnd,
+                int msg,
+                IntPtr wParam,
+                IntPtr lParam);
+
+            [DllImport("user32.dll", EntryPoint = "SendMessage", CharSet = CharSet.Unicode)]
+            internal static extern int SendMessage(IntPtr hwnd,
+                int msg,
+                int wParam,
+                [MarshalAs(UnmanagedType.LPWStr)] StringBuilder lParam);
+
+            [DllImport("user32.dll", EntryPoint = "SendMessage", CharSet = CharSet.Unicode)]
+            internal static extern IntPtr SendMessage(IntPtr hwnd,
+                int msg,
+                IntPtr wParam,
+                String lParam);
         }
     }
 }
--- README.md
+++ README.md
@@ -3,19 +3,22 @@
   <img src="https://github.com/DarkDestry/Cauldron/blob/master/Cauldron/Cauldron.png" alt="Cauldron-Logo" width="200">
   <br>
 </h1>
-<h4 align="center">A WIP scene editor built using WPF and Monogame</h4>
+<h4 align="center">A WIP scene editor built using WPF, SharpGL and Ether</h4>
 
 
 ## Introduction
 
-Cauldron is an editor designed to be used with [Elixir](https://github.com/Eclmist/Elixir). 
+Cauldron is an editor intended to be used with [Elixir](https://github.com/Eclmist/Elixir). 
 
 The Cauldron project started with the need of a scene editor for a headless renderer. It is intended to be an experiment and learning experience for building a editor from the ground up. 
+
+SharpGL is used as an temporary renderer as a separate DX12 renderer is being built.
 
 ## Built With
 
 * [Windows Presentation Foundation](https://docs.microsoft.com/en-us/dotnet/framework/wpf/) - The Framework used for the UI
-* [OpenGL](https://github.com/dwmkerr/sharpgl/wiki) - Raster Renderer for scene rendering
+* [SharpGL](https://github.com/dwmkerr/sharpgl/wiki) - Temporary Raster Renderer for scene rendering
+* [Ether](https://github.com/Eclmist/Ether) - DX12 Raster Renderer for scene rendering
 
 ## Screenshots
 
